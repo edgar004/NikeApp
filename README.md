b62e6e738e7bafd27b3d2f4fdb28dcc2d9f0a744
edgar004 <ariel.rd.004@hotmail.com>
2020-07-11 02:18:39 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Desktop: 1024px
+- Does not yet have support for mobile devices
+
+## Colors
+
+### Primary
+
+- Black: #131313;
+- Gray: #232323;
+- Green: #9BDC28;
+- Blue: #03A9F4;
+- Pink: #E91E63;
+
+## Typography
+
+- Poppins
+- Sans-serif
+
+### Header and Titles
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 800
+
+### Body and Paragraphs
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400
+
+## Images
+
+All the icons used were created in svg format to maintain their size-quality ratio:
+
+- [Original Nike Sneaker](http://pngimg.com/download/5816)
+- [Edit Blue Sneaker](img/shoes-blue.png)
+- [Edit Pink Sneaker](img/shoes-pink.png)
+
+## Copyright
+
+The original design of the cards and everything related to css styles belong to [Online Tutorials](https://www.youtube.com/channel/UCbwXnUipZsLfUckBPsC7Jog)
+
+[Original Card UI Design](https://www.youtube.com/watch?v=jUByQLfakJ8&t)# Nike-App
+# Nike-App
---
+++ js/script.js
@@ -0,0 +1,72 @@
+(function(){
+    'use strict';
+    document.addEventListener('DOMContentLoaded',function(){
+        console.log('Ready!!');
+
+        class Sneaker{
+            constructor(name,size,color){
+                this.name = name
+                this.size = size
+                this.color = color
+            }
+        }
+
+        class Render{
+            renderCard(newSneaker){
+                const productList = document.getElementById('product-list')
+                const element = document.createElement('div')
+                element.innerHTML =
+                `
+                <div class="container">
+                    <div class="card ${newSneaker.color}">
+                        <div class="imgBx">
+                            <img src="img/shoes-${newSneaker.color}.png" alt="shoes">
+                        </div>
+                        <div class="contentBx">
+                            <h2>${newSneaker.name}</h2>
+                            <div class="size ${newSneaker.color}">
+                                <h3>Size :</h3>
+                                <span>${newSneaker.size}</span>
+                            </div>
+                            <div class="color">
+                                <h3>Color :</h3>
+                                <span class="circle-${newSneaker.color}"></span>
+                            </div>
+                            <a href="#">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+                productList.appendChild(element)
+                form.reset()
+            }
+        }
+
+        const form = document.getElementById('productForm')
+        form.addEventListener('submit', function(e){
+
+            const name = document.getElementById('name').value
+            const size = document.getElementById('size').value
+            const colorGreen = document.getElementById('green').checked
+            const colorBlue = document.getElementById('blue').checked
+            const colorPink = document.getElementById('pink').checked
+
+            if(colorGreen){
+                var color = 'green'
+            }
+            if(colorBlue){
+                var color = 'blue'
+            }
+            if(colorPink){
+                var color = 'pink'
+            }
+
+            const newSneaker = new Sneaker(name,size,color)
+
+            const UI = new Render()
+            UI.renderCard(newSneaker)
+
+            e.preventDefault()
+        })
+    });
+})();
